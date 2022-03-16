d38271dd6e491a13f5875e7d35be24df9df78a2e
Michael Phillips <michael@cpsplumbing.net>
2022-03-15 21:50:54 -0400

    Added ability to enter a space with keyboard

--- src/components/InputRow/inputRow.js
+++ src/components/InputRow/inputRow.js
@@ -12,10 +12,12 @@ const InputRow = (props) => {
       if (e.keyCode === 8) {
         const newInputArray = inputRef.current.slice(0, inputRef.current.length - 1);
         setInput(() => newInputArray);
-      } else if (e.keyCode === 13 && inputRef.current.length === 5) {
+      } else if (e.keyCode === 13 && inputRef.current.length === 5 && !inputRef.current.includes(" ")) {
         props.handleGuess(inputRef.current);
       } else if (inputRef.current.length < 5 && e.keyCode > 64 && e.keyCode < 91) {
         setInput((prev) => [...prev, e.key.toUpperCase()]);
+      } else if (inputRef.current.length < 5 && e.keyCode === 32) {
+        setInput((prev) => [...prev, e.key]);
       }
     }
     if (props.active) {
@@ -32,11 +34,41 @@ const InputRow = (props) => {
   return (
     <>
       <div className={styles.container}>
-        <div className={`${styles.letterBox} ${styles[props.rowColors[0]]} ${inputRef.current.length > 0 ? styles.filled : ""}`}>{input[0]}</div>
-        <div className={`${styles.letterBox} ${styles[props.rowColors[1]]} ${inputRef.current.length > 1 ? styles.filled : ""}`}>{input[1]}</div>
-        <div className={`${styles.letterBox} ${styles[props.rowColors[2]]} ${inputRef.current.length > 2 ? styles.filled : ""}`}>{input[2]}</div>
-        <div className={`${styles.letterBox} ${styles[props.rowColors[3]]} ${inputRef.current.length > 3 ? styles.filled : ""}`}>{input[3]}</div>
-        <div className={`${styles.letterBox} ${styles[props.rowColors[4]]} ${inputRef.current.length > 4 ? styles.filled : ""}`}>{input[4]}</div>
+        <div
+          className={`${styles.letterBox} ${styles[props.rowColors[0]]} ${
+            inputRef.current.length > 0 ? styles.filled : ""
+          }`}
+        >
+          {input[0]}
+        </div>
+        <div
+          className={`${styles.letterBox} ${styles[props.rowColors[1]]} ${
+            inputRef.current.length > 1 ? styles.filled : ""
+          }`}
+        >
+          {input[1]}
+        </div>
+        <div
+          className={`${styles.letterBox} ${styles[props.rowColors[2]]} ${
+            inputRef.current.length > 2 ? styles.filled : ""
+          }`}
+        >
+          {input[2]}
+        </div>
+        <div
+          className={`${styles.letterBox} ${styles[props.rowColors[3]]} ${
+            inputRef.current.length > 3 ? styles.filled : ""
+          }`}
+        >
+          {input[3]}
+        </div>
+        <div
+          className={`${styles.letterBox} ${styles[props.rowColors[4]]} ${
+            inputRef.current.length > 4 ? styles.filled : ""
+          }`}
+        >
+          {input[4]}
+        </div>
       </div>
     </>
   );
